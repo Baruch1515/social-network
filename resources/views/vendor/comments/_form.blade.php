664ac4beaea66131c8c4670ce310432b96d0c8f9
Baruch13 <ernestobaruch15@gmail.com>
2021-06-07 07:11:25 -0500

    Errores en vista de publicacion

--- resources/views/vendor/comments/_form.blade.php
+++ resources/views/vendor/comments/_form.blade.php
@@ -1,33 +1,25 @@
-
-<!-- JavaScript Bundle with Popper -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-<!-- CSS only -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-<x-app-layout>
 <div class="card">
     <div class="card-body">
         @if($errors->has('commentable_type'))
             <div class="alert alert-danger" role="alert">
-                {{ $errors->first('commentable_type') }}
+                {{ $errors->get('commentable_type') }}
             </div>
         @endif
         @if($errors->has('commentable_id'))
             <div class="alert alert-danger" role="alert">
-                {{ $errors->first('commentable_id') }}
+                {{ $errors->get('commentable_id') }}
             </div>
         @endif
-        <form method="POST" action="{{ route('comments.store') }}">
+        <form method="POST" action="{{ url('comments') }}">
             @csrf
             @honeypot
             <input type="hidden" name="commentable_type" value="\{{ get_class($model) }}" />
-            <input type="hidden" name="commentable_id" value="{{ $model->getKey() }}" />
+            <input type="hidden" name="commentable_id" value="{{ $model->id }}" />
 
             {{-- Guest commenting --}}
             @if(isset($guest_commenting) and $guest_commenting == true)
                 <div class="form-group">
-                    <label for="message">@lang('comments::comments.enter_your_name_here')</label>
+                    <label for="message">Enter your name here:</label>
                     <input type="text" class="form-control @if($errors->has('guest_name')) is-invalid @endif" name="guest_name" />
                     @error('guest_name')
                         <div class="invalid-feedback">
@@ -36,7 +28,7 @@
                     @enderror
                 </div>
                 <div class="form-group">
-                    <label for="message">@lang('comments::comments.enter_your_email_here')</label>
+                    <label for="message">Enter your email here:</label>
                     <input type="email" class="form-control @if($errors->has('guest_email')) is-invalid @endif" name="guest_email" />
                     @error('guest_email')
                         <div class="invalid-feedback">
@@ -47,14 +39,14 @@
             @endif
 
             <div class="form-group">
-                <label for="message">@lang('comments::comments.enter_your_message_here')</label>
+                <label for="message">Enter your message here:</label>
                 <textarea class="form-control @if($errors->has('message')) is-invalid @endif" name="message" rows="3"></textarea>
                 <div class="invalid-feedback">
-                    @lang('comments::comments.your_message_is_required')
+                    Your message is required.
                 </div>
-                <small class="form-text text-muted">@lang('comments::comments.markdown_cheatsheet', ['url' => 'https://help.github.com/articles/basic-writing-and-formatting-syntax'])</small>
+                <small class="form-text text-muted"><a target="_blank" href="https://help.github.com/articles/basic-writing-and-formatting-syntax">Markdown</a> cheatsheet.</small>
             </div>
-            <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">@lang('comments::comments.submit')</button>
+            <button type="submit" class="btn btn-sm btn-outline-success text-uppercase">Submit</button>
         </form>
     </div>
 </div>
